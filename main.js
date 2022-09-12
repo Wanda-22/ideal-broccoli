c2b3587cc427364d51a7c2df455fbd4b6e82388c
Wanda-22 <105216373+Wanda-22@users.noreply.github.com>
2022-09-12 16:54:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+x=[];
+function a(){
+    var y=document.getElementById("i1").value;
+    var y1=document.getElementById("i2").value;
+    var y2=document.getElementById("i3").value;
+    var y3=document.getElementById("i4").value;
+
+    x.push(y);
+    x.push(y1);
+    x.push(y2);
+    x.push(y3);
+    
+    console.log(x);
+    document.getElementById("d").innerHTML = x;
+document.getElementById("s").style.display = "none";
+document.getElementById("so").style.display = "inline-block";
+}
+function a1(){
+    x.sort();
+    console.log(x);
+    document.getElementById("d").innerHTML = x;
+}
